f05b73c7f2f1f7bb84fe24c3213ca9924b0118e8
POKE17104 <hahd5604@ut.edu.vn>
2024-10-28 02:24:44 +0700

    Delete Solution_KOI directory

--- Solution_KOI/Presentation_Layer_KOI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Presentation_Layer_KOI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
